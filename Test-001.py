b68a90778c330377ba3a7beade5ae10d891c38fc
Thegg0 <sashacireddu2004@gmail.com>
2024-06-27 12:16:56 +0200

    v 0.0.1

--- Test-001.py
+++ Test-001.py
@@ -1,4 +1,20 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
+from scipy import stats
+from sklearn.metrics import r2_score
 
-x = numpy.random.uniform(0.0, 5.0, 250)
+X = [12, 21, 22, 23, 24, 18, 16, 23, 14, 18]
+y = [22, 25, 27, 22, 34, 18, 19, 20, 23, 26]
+
+slope, intercept, r, p , std_err = stats.linregress(X,y)
+
+def myfunc(X):
+    return slope * X + intercept
+
+mymodel = list(map(myfunc, X))
+
+plt.scatter(X,y)
+plt.plot(X, mymodel)
+
+print(r2_score(y, mymodel))
+
